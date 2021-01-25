4ec1066199f065907f85f03d32a1864e2f60e6fc
Yeri <arifyeripratama@gmail.com>
2021-01-25 10:20:32 +0700

    instalasi adminlte

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -8,11 +8,6 @@ class ExampleController extends Controller
 {
     public function index()
     {
-        return view('welcome');
-    }
-
-    public function about()
-    {
-        return view('about');
+        return view('starter');
     }
 }
